e07ee8a6fa66002032c9ca49bf574f7373410cf7
Alberto <Albertopga@gmail.com>
2020-12-21 08:34:15 +0100

    redirect instead of link

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -1,12 +1,13 @@
 import React from "react";
 import User from "../User/User";
+import { Redirect } from "react-router";
 
 /**
  * Receive the search results and the errors, by the parent component and then displayed them.
  */
 const ListResults = ({ users, error }) => {
-  const perseUser = () => {
-    if (users.length === 1) return <User key={users[0].id} user={users[0]} />;
+  const parseUser = () => {
+    if (users.length === 1) return <Redirect to={`find/${users[0].id}`} />;
 
     return users.map((user) => (
       <User key={user.id} user={{ name: user.name, id: user.id }} />
@@ -15,7 +16,7 @@ const ListResults = ({ users, error }) => {
 
   return (
     <div className="d-flex flex-sm-wrap justify-content-center ">
-      {users && perseUser()}
+      {users && parseUser()}
       {error && <h5>{error.message}</h5>}
     </div>
   );
