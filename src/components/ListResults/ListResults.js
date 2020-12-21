d15a90021a58112b9b087e018c4c0eccf06d30c2
Alberto <Albertopga@gmail.com>
2020-12-21 07:57:48 +0100

    error typo

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -5,22 +5,13 @@ import User from "../User/User";
  * Receive the search results and the errors, by the parent component and then displayed them.
  */
 const ListResults = ({ users, error }) => {
-  const parseErrorMessages = () => {
-    if (error) {
-    }
-  };
-
   return (
     <div className="d-flex flex-sm-wrap justify-content-center ">
       {users &&
         users.map((user) => (
           <User key={user.id} user={{ name: user.name, id: user.id }} />
         ))}
-      {error && (
-        <h5>
-          {parseErrorMessages()} {error.message}
-        </h5>
-      )}
+      {error && <h5>{error.message}</h5>}
     </div>
   );
 };
